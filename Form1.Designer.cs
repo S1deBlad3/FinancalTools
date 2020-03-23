d61394d028ec489ab631bfc17b2aaf495547cb85
S1deBlad3 <mariuspedersen83@gmail.com>
2020-03-23 17:39:29 +0100

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,419 @@
+﻿namespace Windows_form_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.UnderlagBox = new System.Windows.Forms.TextBox();
+            this.FörsäljningBox = new System.Windows.Forms.TextBox();
+            this.InköpBox = new System.Windows.Forms.TextBox();
+            this.SkatteBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IBInventatrieBox = new System.Windows.Forms.TextBox();
+            this.ÖveravskrivningarBox = new System.Windows.Forms.TextBox();
+            this.PlanenligaBox = new System.Windows.Forms.TextBox();
+            this.AnskaffingBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.ResultatBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.MaxÖveravskriningBox = new System.Windows.Forms.TextBox();
+            this.UderlagBox2 = new System.Windows.Forms.TextBox();
+            this.UBAckBox = new System.Windows.Forms.TextBox();
+            this.UBBOX = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.IBöverack = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(387, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Underlag 30%";
+            // 
+            // UnderlagBox
+            // 
+            this.UnderlagBox.Location = new System.Drawing.Point(371, 171);
+            this.UnderlagBox.Name = "UnderlagBox";
+            this.UnderlagBox.Size = new System.Drawing.Size(100, 20);
+            this.UnderlagBox.TabIndex = 13;
+            // 
+            // FörsäljningBox
+            // 
+            this.FörsäljningBox.Location = new System.Drawing.Point(417, 97);
+            this.FörsäljningBox.Name = "FörsäljningBox";
+            this.FörsäljningBox.Size = new System.Drawing.Size(100, 20);
+            this.FörsäljningBox.TabIndex = 11;
+            // 
+            // InköpBox
+            // 
+            this.InköpBox.Location = new System.Drawing.Point(417, 71);
+            this.InköpBox.Name = "InköpBox";
+            this.InköpBox.Size = new System.Drawing.Size(100, 20);
+            this.InköpBox.TabIndex = 10;
+            // 
+            // SkatteBox
+            // 
+            this.SkatteBox.Location = new System.Drawing.Point(417, 45);
+            this.SkatteBox.Name = "SkatteBox";
+            this.SkatteBox.Size = new System.Drawing.Size(100, 20);
+            this.SkatteBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "IB Inventarie";
+            // 
+            // IBInventatrieBox
+            // 
+            this.IBInventatrieBox.Location = new System.Drawing.Point(77, 171);
+            this.IBInventatrieBox.Name = "IBInventatrieBox";
+            this.IBInventatrieBox.Size = new System.Drawing.Size(100, 20);
+            this.IBInventatrieBox.TabIndex = 19;
+            // 
+            // ÖveravskrivningarBox
+            // 
+            this.ÖveravskrivningarBox.Location = new System.Drawing.Point(162, 97);
+            this.ÖveravskrivningarBox.Name = "ÖveravskrivningarBox";
+            this.ÖveravskrivningarBox.Size = new System.Drawing.Size(100, 20);
+            this.ÖveravskrivningarBox.TabIndex = 17;
+            // 
+            // PlanenligaBox
+            // 
+            this.PlanenligaBox.Location = new System.Drawing.Point(162, 71);
+            this.PlanenligaBox.Name = "PlanenligaBox";
+            this.PlanenligaBox.Size = new System.Drawing.Size(100, 20);
+            this.PlanenligaBox.TabIndex = 16;
+            // 
+            // AnskaffingBox
+            // 
+            this.AnskaffingBox.Location = new System.Drawing.Point(162, 45);
+            this.AnskaffingBox.Name = "AnskaffingBox";
+            this.AnskaffingBox.Size = new System.Drawing.Size(100, 20);
+            this.AnskaffingBox.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Anskaffningsvärde";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Ack. Planenliga avskivningar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Ack överavskrivningar";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(330, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "IB inventarie";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(330, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Årets inköp";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(330, 97);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 13);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "Årets försäljning";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(15, 408);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(487, 17);
+            this.hScrollBar1.TabIndex = 29;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // ResultatBox
+            // 
+            this.ResultatBox.Location = new System.Drawing.Point(182, 428);
+            this.ResultatBox.Name = "ResultatBox";
+            this.ResultatBox.Size = new System.Drawing.Size(160, 20);
+            this.ResultatBox.TabIndex = 31;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(103, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 13);
+            this.label11.TabIndex = 32;
+            this.label11.Text = "IB Inventarie";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(403, 9);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(72, 13);
+            this.label12.TabIndex = 33;
+            this.label12.Text = "Skrivunderlag";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "Calc";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(390, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 35;
+            this.button3.Text = "Calc";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(225, 352);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 44;
+            this.button4.Text = "Calc";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(159, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 43;
+            this.label6.Text = "Underlag";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(159, 233);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(43, 13);
+            this.label10.TabIndex = 42;
+            this.label10.Text = "UB ack";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(173, 207);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(22, 13);
+            this.label13.TabIndex = 41;
+            this.label13.Text = "UB";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(207, 310);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(108, 13);
+            this.label14.TabIndex = 40;
+            this.label14.Text = "MAX överavskrivning";
+            // 
+            // MaxÖveravskriningBox
+            // 
+            this.MaxÖveravskriningBox.Location = new System.Drawing.Point(210, 326);
+            this.MaxÖveravskriningBox.Name = "MaxÖveravskriningBox";
+            this.MaxÖveravskriningBox.Size = new System.Drawing.Size(100, 20);
+            this.MaxÖveravskriningBox.TabIndex = 39;
+            // 
+            // UderlagBox2
+            // 
+            this.UderlagBox2.Location = new System.Drawing.Point(284, 252);
+            this.UderlagBox2.Name = "UderlagBox2";
+            this.UderlagBox2.Size = new System.Drawing.Size(100, 20);
+            this.UderlagBox2.TabIndex = 38;
+            // 
+            // UBAckBox
+            // 
+            this.UBAckBox.Location = new System.Drawing.Point(284, 226);
+            this.UBAckBox.Name = "UBAckBox";
+            this.UBAckBox.Size = new System.Drawing.Size(100, 20);
+            this.UBAckBox.TabIndex = 37;
+            // 
+            // UBBOX
+            // 
+            this.UBBOX.Location = new System.Drawing.Point(284, 200);
+            this.UBBOX.Name = "UBBOX";
+            this.UBBOX.Size = new System.Drawing.Size(100, 20);
+            this.UBBOX.TabIndex = 36;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(159, 285);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(96, 13);
+            this.label15.TabIndex = 46;
+            this.label15.Text = "IB ack överavskriv";
+            // 
+            // IBöverack
+            // 
+            this.IBöverack.Location = new System.Drawing.Point(284, 278);
+            this.IBöverack.Name = "IBöverack";
+            this.IBöverack.Size = new System.Drawing.Size(100, 20);
+            this.IBöverack.TabIndex = 45;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 484);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.IBöverack);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.MaxÖveravskriningBox);
+            this.Controls.Add(this.UderlagBox2);
+            this.Controls.Add(this.UBAckBox);
+            this.Controls.Add(this.UBBOX);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.ResultatBox);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IBInventatrieBox);
+            this.Controls.Add(this.ÖveravskrivningarBox);
+            this.Controls.Add(this.PlanenligaBox);
+            this.Controls.Add(this.AnskaffingBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.UnderlagBox);
+            this.Controls.Add(this.FörsäljningBox);
+            this.Controls.Add(this.InköpBox);
+            this.Controls.Add(this.SkatteBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox UnderlagBox;
+        private System.Windows.Forms.TextBox FörsäljningBox;
+        private System.Windows.Forms.TextBox InköpBox;
+        private System.Windows.Forms.TextBox SkatteBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IBInventatrieBox;
+        private System.Windows.Forms.TextBox ÖveravskrivningarBox;
+        private System.Windows.Forms.TextBox PlanenligaBox;
+        private System.Windows.Forms.TextBox AnskaffingBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.TextBox ResultatBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox MaxÖveravskriningBox;
+        private System.Windows.Forms.TextBox UderlagBox2;
+        private System.Windows.Forms.TextBox UBAckBox;
+        private System.Windows.Forms.TextBox UBBOX;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox IBöverack;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Windows_form_2
+{
+    public partial class Form1 : Form
+    {
+
+        public static int anskaffning, ackPlan, ackÖver;
+
+        public static int IBinv;
+
+        public static int inköp, försäljning;
+
+        public static double underlag;
+
+
+
+        public static double resultat;
+
+
+        public static int UBInventarie, UBack;
+
+        public static double MaxÖveravskrivningar;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            anskaffning = Convert.ToInt32(AnskaffingBox.Text);
+            ackPlan = Convert.ToInt32(PlanenligaBox.Text);
+            ackÖver = Convert.ToInt32(ÖveravskrivningarBox.Text);
+
+            IBinv = anskaffning - ackPlan - ackÖver;
+
+            IBInventatrieBox.Text = Convert.ToString(IBinv);
+            SkatteBox.Text = Convert.ToString(IBinv);
+
+
+        }
+
+      
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            inköp = Convert.ToInt32(InköpBox.Text);
+            försäljning = Convert.ToInt32(FörsäljningBox.Text);
+
+            underlag = (IBinv + inköp - försäljning) * 0.70;
+
+            UnderlagBox.Text = Convert.ToString(underlag);
+            IBöverack.Text = Convert.ToString(ackÖver);
+            UderlagBox2.Text = Convert.ToString(underlag);
+            UBInventarie = IBinv + inköp;
+            UBBOX.Text = Convert.ToString(UBInventarie);
+
+        }
+
+
+
+       
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            IBöverack.Text = Convert.ToString(ackÖver);
+
+           
+            UBack = Convert.ToInt32(UBAckBox.Text);
+
+
+
+            MaxÖveravskrivningar = UBInventarie - UBack - underlag - ackÖver;
+
+            
+
+            
+
+            resultat = MaxÖveravskrivningar;
+
+            MaxÖveravskriningBox.Text = Convert.ToString(resultat);
+
+
+            ResultatBox.Text = Convert.ToString(resultat);
+
+            hScrollBar1.Minimum = 0;
+            hScrollBar1.Maximum = Convert.ToInt32(resultat);
+            
+
+        }
+
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+            resultat = hScrollBar1.Value;
+
+            ResultatBox.Text = Convert.ToString(resultat);
+
+
+
+
+
+
+        }
+
+
+    }
+
+}
